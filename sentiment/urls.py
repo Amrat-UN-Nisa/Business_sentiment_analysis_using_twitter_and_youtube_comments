fc4dd2d4c7cf59ddb2dfe8bcf859e54919c4e324
Amrat-glowlogix <amratnisa99@gmail.com>
2023-05-05 09:16:30 +0500

    first commit

---
+++ sentiment/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from sentiment import views
+
+urlpatterns = [
+    path('login/', views.login_function, name='login'),
+    path('sign-up/', views.sign_up, name='sign-up'),
+    path('index/', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('youtube-result/', views.youtube_result, name='youtube-result'),
+    path('twitter-result/', views.twitter_result, name='twitter-result'),
+]
